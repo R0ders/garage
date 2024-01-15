9c1bc80046bb1b14b0a893357673096126fb14c9
R0ders <156414966+R0ders@users.noreply.github.com>
2024-01-15 14:00:43 +0100

    Update garage.cpp

--- garage/garage.cpp
+++ garage/garage.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include "Vehicule.h"
+#include "Voiture.h"
 using namespace std;
+
+void AfficheMenu()
+{
+	cout << "1) Informations du vehicule" << endl;
+	cout << "2) Le type du vehicule" << endl;
+	cout << "3) Quitter" << endl;
+}
 
 int main()
 {
+	string Vmarque, Vtype;
+	int choix, Vpuissance, Vdist;
+
 	
 }
