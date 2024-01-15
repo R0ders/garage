b3ecb38f63b4d76da8e13a6b4832a7a8e6b399dd
R0ders <156414966+R0ders@users.noreply.github.com>
2024-01-15 14:57:32 +0100

    modification du code de garage.cpp

--- garage/garage.cpp
+++ garage/garage.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void AfficheMenu()
 {
-	cout << "1) Informations du vehicule" << endl;
+	cout << "1) Les informations du vehicule" << endl;
 	cout << "2) Le type du vehicule" << endl;
 	cout << "3) Quitter" << endl;
 }
@@ -17,4 +17,47 @@ int main()
 
 	do {
 		AfficheMenu();
+
+		cout << "Quel information souhaitez vous ?" << endl;
+		cin >> choix;
+
+		switch(choix)
+			{
+				case 1:
+					cout << "Information du vehicule" << endl;
+					cout << "marque du vehicule : ";
+					cin << Vmarque;
+					cout << "puissance du vehicule : ";
+					cin << Vpuissance;
+					cout << "Distance parcouru par le vehicule : ";
+					cin << Vdist;
+
+					Vehicule(Vmarque, Vpuissance, Vdist);
+					Vehicule(lireCaracteristique());
+
+				break();
+
+				case 2:
+					cout << "Le type du vehicule" << endl;
+					cout << "Type du vehicule : ";
+					cin >> Vtype;
+
+					Voiture(Vmarque, Vpuissance, Vdist, Vtype);
+					Voiture(lireType());
+
+				break();
+
+				case 3:
+					cout << "Au revoir et merci pour votre connexion" << endl;
+				
+				break();
+
+				default:
+					cout << "La formation demander est inaccessible" << endl;
+
+				break();
+	} while(choix =! 3)
+			{
+				return 0;
+			}
 }
